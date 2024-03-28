062f37f6339abed82c35275fa7b94251185a858d
BigBabyofTel <augustus.tb@outlook.com>
2024-03-28 18:18:48 +0200

    feat: post request validation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import { Hono } from 'hono';
 import { ENV } from './lib/env';
+import { z, ZodError } from 'zod';
 
 const app = new Hono();
+
+const validateUser = z.object({
+  name: z.string().min(3, 'Please provide 3 characters').trim(),
+  age: z.number().min(1).max(100),
+  email: z.string().email(),
+});
+
+app.post('/', async (c) => {
+  const body = await c.req.json();
+
+  try {
+    const { age, name } = validateUser.parse(body);
+  } catch (e) {
+    if (e instanceof ZodError) {
+        console.log(e.errors)
+        const arr = []
+        for (let error of e.errors) {
+          const errObj: Record<string, string> = {};
+          const key = error.path[0];
+          errObj[key] = error.message;
+          arr.push(errObj)
+        }
+
+      c.status(422);
+      return c.json({ errors: arr })
+    }
+    c.status(500)
+    return c.json({ msg: 'something went wrong' });
+  }
+  return c.json(body);
+});
 
 app.get('/', (c) => {
   console.log(process.env.PORT);
